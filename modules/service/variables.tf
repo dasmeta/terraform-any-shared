35e0228259b566871118650888e40cfba1994301
Aram Karapetyan <aram@dasmeta.com>
2025-02-06 10:40:03 +0400

    fix(DMVP-6631): Add repo and chart variables

--- modules/service/variables.tf
+++ modules/service/variables.tf
@@ -12,3 +12,15 @@ variable "helm_values" {
   type        = any
   description = "Values which is overwrite chart defaults"
 }
+
+variable "chart" {
+  type        = string
+  default     = "base"
+  description = "The name of the Helm chart to deploy."
+}
+
+variable "repository" {
+  type        = string
+  default     = "https://dasmeta.github.io/helm/"
+  description = "The URL of the Helm chart repository."
+}

9903de8ce3eb6bb3d2bb43e7c84349f308858a82
Vazgen Sargsyan <vazgen@dasmeta.com>
2026-02-24 18:24:14 +0400

    fix(DMVP-9447): Added Terraform Helm module for DefectDojo

---
+++ modules/defectdojo/main.tf
@@ -0,0 +1,16 @@
+locals {
+  helm_values = merge(var.default_configs, var.custom_configs)
+}
+
+resource "helm_release" "this" {
+  name             = var.name
+  repository       = "https://raw.githubusercontent.com/DefectDojo/django-DefectDojo/helm-charts"
+  chart            = "defectdojo"
+  namespace        = var.namespace
+  version          = var.chart_version
+  create_namespace = var.create_namespace
+  atomic           = var.atomic
+  wait             = var.wait
+
+  values = [jsonencode(local.helm_values)]
+}

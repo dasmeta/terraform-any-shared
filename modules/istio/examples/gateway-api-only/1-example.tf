d81f4803b9ae1dea690dd7a364682e56e3a75acd
Tigran Muradyan <tigran@dasmeta.com>
2026-03-04 14:17:24 +0400

    fix(DMVP-9129): refactor istio module variables

--- modules/istio/examples/gateway-api-only/1-example.tf
+++ modules/istio/examples/gateway-api-only/1-example.tf
@@ -13,90 +13,92 @@ module "this" {
   configs = {
     # base = { enabled = false } # todo: enable this
     gateway = {
-      ingressgateway = {
+      ingress_gateway = {
         enabled = false
       }
       # Enable Gateway API resources (native k8s Gateway objects)
-      resources = [
-        {
-          name             = "main"
-          gatewayClassName = "istio"
-          listeners = [
-            # http-echo-gateway-api.localhost
-            {
-              name     = "http-echo-gateway-api-http"
-              hostname = "http-echo-gateway-api.localhost"
-              port     = 80
-              protocol = "HTTP"
-              allowedRoutes = {
-                namespaces = {
-                  from = "All"
+      api_resources = {
+        gateways = [
+          {
+            name             = "main"
+            gatewayClassName = "istio"
+            listeners = [
+              # http-echo-gateway-api.localhost
+              {
+                name     = "http-echo-gateway-api-http"
+                hostname = "http-echo-gateway-api.localhost"
+                port     = 80
+                protocol = "HTTP"
+                allowedRoutes = {
+                  namespaces = {
+                    from = "All"
+                  }
+                }
+              },
+              # http-echo-gw.localhost
+              {
+                name     = "http-echo-gw-http"
+                port     = 80
+                protocol = "HTTP"
+                hostname = "http-echo-gw.localhost"
+                allowedRoutes = {
+                  namespaces = {
+                    from = "All"
+                  }
+                }
+              },
+              # redirect-example.localhost
+              {
+                name     = "redirect-example"
+                port     = 80
+                protocol = "HTTP"
+                hostname = "redirect-example.localhost"
+                allowedRoutes = {
+                  namespaces = {
+                    from = "All"
+                  }
+                }
+              },
+              # forbidden-example.localhost
+              {
+                name     = "forbidden-example"
+                port     = 80
+                protocol = "HTTP"
+                hostname = "forbidden-example.localhost"
+                allowedRoutes = {
+                  namespaces = {
+                    from = "All"
+                  }
+                }
+              },
+              # not-found-example.localhost
+              {
+                name     = "not-found-example"
+                port     = 80
+                protocol = "HTTP"
+                hostname = "not-found-example.localhost"
+                allowedRoutes = {
+                  namespaces = {
+                    from = "All"
+                  }
+                }
+              },
+              # virtualservice-example.localhost
+              {
+                name     = "virtualservice-example"
+                port     = 80
+                protocol = "HTTP"
+                hostname = "virtualservice-example.localhost"
+                allowedRoutes = {
+                  namespaces = {
+                    from = "All"
+                  }
                 }
               }
-            },
-            # http-echo-gw.localhost
-            {
-              name     = "http-echo-gw-http"
-              port     = 80
-              protocol = "HTTP"
-              hostname = "http-echo-gw.localhost"
-              allowedRoutes = {
-                namespaces = {
-                  from = "All"
-                }
-              }
-            },
-            # redirect-example.localhost
-            {
-              name     = "redirect-example"
-              port     = 80
-              protocol = "HTTP"
-              hostname = "redirect-example.localhost"
-              allowedRoutes = {
-                namespaces = {
-                  from = "All"
-                }
-              }
-            },
-            # forbidden-example.localhost
-            {
-              name     = "forbidden-example"
-              port     = 80
-              protocol = "HTTP"
-              hostname = "forbidden-example.localhost"
-              allowedRoutes = {
-                namespaces = {
-                  from = "All"
-                }
-              }
-            },
-            # not-found-example.localhost
-            {
-              name     = "not-found-example"
-              port     = 80
-              protocol = "HTTP"
-              hostname = "not-found-example.localhost"
-              allowedRoutes = {
-                namespaces = {
-                  from = "All"
-                }
-              }
-            },
-            # virtualservice-example.localhost
-            {
-              name     = "virtualservice-example"
-              port     = 80
-              protocol = "HTTP"
-              hostname = "virtualservice-example.localhost"
-              allowedRoutes = {
-                namespaces = {
-                  from = "All"
-                }
-              }
-            }
-          ]
-        }
-      ]
+            ]
+          }
+        ]
+      }
     }
     # Istiod configuration - disable automatic sidecar injection
     istiod = {
